0bb4f1f29372f4e2369a54daf772b7a4376c160f
Athea DeLing <almdeling@gmail.com>
2022-08-26 10:42:49 -0700

    work on UI for each selection

--- js/scripts.js
+++ js/scripts.js
@@ -16,11 +16,17 @@ function handleFormSubmission(event) {
   const selectedSize = document.getElementById("size").value;
   document.querySelector("size").innerText = selectedSize;
   const selectedCrust = document.getElementById("crust").value;
+  document.querySelector("crust").innerText = selectedCrust;
   const selectedCheese = document.getElementById("cheese").value;
+  document.querySelector("cheese").innerText = selectedCheese;
   const selectedMeat = document.getElementById("meat").value;
+  document.querySelector("meat").innerText = selectedSize;
   const selectedSauce = document.getElementById("sauce").value;
+  document.querySelector("sauce").innerText = selectedSauce;
   const selectedGreens = document.getElementById("greens").value;
+  document.querySelector("greens").innerText = selectedGreens;
   const selectedExtras = document.getElementById("extras").value;
+  document.querySelector("extras").innerText = selectedExtras;
 }
 
 window.addEventListener("load", function() {
