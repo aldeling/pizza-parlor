de255e392b8db4e459b16b944537cacb2f619571
Athea DeLing <almdeling@gmail.com>
2022-08-26 11:08:01 -0700

    items show up on finale receipt

--- js/scripts.js
+++ js/scripts.js
@@ -13,20 +13,20 @@ function Pizza(size, crust, cheese, meat, sauce, greens, extras) {
 // UI Logic
 function handleFormSubmission(event) {
   event.preventDefault();
-  const selectedSize = document.getElementById("size").value;
-  document.querySelector("size").innerText = selectedSize;
+  let selectedSize = document.getElementById("size").value;
+  document.querySelector(".size").innerText = selectedSize;
   const selectedCrust = document.getElementById("crust").value;
-  document.querySelector("crust").innerText = selectedCrust;
+  document.querySelector(".crust").innerText = selectedCrust;
   const selectedCheese = document.getElementById("cheese").value;
-  document.querySelector("cheese").innerText = selectedCheese;
+  document.querySelector(".cheese").innerText = selectedCheese;
   const selectedMeat = document.getElementById("meat").value;
-  document.querySelector("meat").innerText = selectedSize;
+  document.querySelector(".meat").innerText = selectedMeat;
   const selectedSauce = document.getElementById("sauce").value;
-  document.querySelector("sauce").innerText = selectedSauce;
+  document.querySelector(".sauce").innerText = selectedSauce;
   const selectedGreens = document.getElementById("greens").value;
-  document.querySelector("greens").innerText = selectedGreens;
+  document.querySelector(".greens").innerText = selectedGreens;
   const selectedExtras = document.getElementById("extras").value;
-  document.querySelector("extras").innerText = selectedExtras;
+  document.querySelector(".extras").innerText = selectedExtras;
 }
 
 window.addEventListener("load", function() {
