eb2c6524dbadf838bf9cd8eb3d29cd2608a3d50d
Athea DeLing <almdeling@gmail.com>
2022-08-26 09:00:44 -0700

    create pizza constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 // Business Logic
-
+function Pizza(size, crust, cheese, meat, sauce, greens, extras) {
+  this.size = size;
+  this.crust = crust;
+  this.cheese = cheese;
+  this.meat = meat;
+  this.sauce = sauce;
+  this.greens = greens;
+  this.extras = extras;
+}
 // UI Logic
